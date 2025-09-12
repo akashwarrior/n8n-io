2f6858fbef0657613e3a43446ae26ef5e8263fac
Akash Gupta <akashwarrior1@gmail.com>
2025-09-12 21:44:27 +0530

    init: empty turborepo project

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Workflow Automation - n8n",
+  description: "Automate worflows using n8n",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1>
+      n8n - workflow automation
+    </h1>
+  );
+}
